53a08fae0aa0c2d8f8a28da2c118169cf750835e
Cristi Dumitrescu <dumi_cristi@yahoo.com>
2023-12-13 00:24:22 +0200

    make it fail

--- tema.cpp
+++ tema.cpp
@@ -18,7 +18,7 @@ bool testMaxInt() {
 		funcToTestAddingTwoNums(MAXINT, MAXINT);
 	}
 	catch(string problemCode) {
-		cout << "Exception thrown. Code:" << problemCode;
+		cout << "Exception thrown. Code:" << problemCode << endl;
 		return 1;
 	}
 	return 0;
@@ -27,18 +27,32 @@ bool testMaxInt() {
 
 int main(int argc, char** argv) {
 	if (argc > 1) {
-		char* whatTestToRun = argv[0];
-		int whatTestToRunInt = *whatTestToRun-'0';
-		cout << whatTestToRun;
+		char* whatTestToRun = argv[1];
 
+		if (*whatTestToRun == '1') {
+			bool testMaxIntResult = testMaxInt();
+			if (testMaxIntResult == 1) {
+				return 1;
+			}
+		}
 
-		bool testMaxIntResult = testMaxInt();
-		if (testMaxIntResult == 1) {
+		if (*whatTestToRun == '2') {
+			cout << "Not Implemented yet";
+			return 0;
+		}
+
+		if (*whatTestToRun == '3') {
+			cout << "Not Implemented yet";
+			return 0;
+		}
+
+		if (*whatTestToRun != '1' || *whatTestToRun != '2' || *whatTestToRun != '3') {
+			cout << "Wrong test. choose 1, 2 or 3";
 			return 1;
 		}
 	}
 	else {
-		cout << "No arguments found";
+		cout << "No arguments found" << endl;
 		return 1;
 	}
 
